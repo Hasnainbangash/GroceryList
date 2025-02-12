7d02f3d75ed38744fb73d647e669f489375a0b46
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-12 10:51:54 +0500

    Initial Commit

---
+++ GroceryList/GroceryListApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryListApp.swift
+//  GroceryList
+//
+//  Created by Elexoft on 12/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct GroceryListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
